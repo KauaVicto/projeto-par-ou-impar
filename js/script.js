97b70a9f85922c4197c5f584795b58afa5725da9
Kauã Victor <kauavictor259@gmail.com>
2021-10-27 21:08:57 -0300

    melhorado

--- js/script.js
+++ js/script.js
@@ -6,20 +6,18 @@ $(document).ready(function () {
     $('.campo_input').hide()
 
 
-    $('#reiniciar').on('click', function (e) {
+    $('#reiniciar').on('click', function (e) { // Ação de clique de Nova partida
         e.preventDefault
 
-        if (!pronto_iniciar) {
+        if (!pronto_iniciar) { // Caso o botão ainda não foi apertado irá realizar essas funções
             $('#iniciar').show()
             $('.campo_input').show()
             $('#valor_usuario')
                 .prop('disabled', false)
                 .val('')
-            $('.resultado span#valor1').html('__')
-            $('.resultado span#valor2').html('__')
-            $('.resultado span#result').html('__')
+            $('.resultado').html('<span id="valor1">__</span> + <span id="valor2">__</span> = <span id="result">__</span>')
 
-            pronto_iniciar = !pronto_iniciar
+            pronto_iniciar = !pronto_iniciar // O jogo se torna pronto para iniciar
 
         }
 
@@ -33,7 +31,7 @@ $(document).ready(function () {
 
             alert('Digite um valor no campo!')
 
-        } else {
+        } else if(valor_usuario != '' && pronto_iniciar) {
 
             if (valor_usuario >= 0 && valor_usuario <= 10) { // Verifica se o valor está entre 0 e 10
 
@@ -41,38 +39,37 @@ $(document).ready(function () {
                 pronto_iniciar = !pronto_iniciar
 
                 valor_pc = Math.round(Math.random() * 10)  // Valor aleatório entre 0 e 10
-                resultado = valor_usuario + valor_pc
+                resultado = valor_usuario + valor_pc // Soma os dois valores
 
-                $('.resultado span#valor1').hide().html(valor_usuario).fadeIn(1000)
+                $('.resultado span#valor1').hide().html(valor_usuario).fadeIn(1000) // Apresenta o valor do usuário
 
                 setTimeout(function () {
-                    $('.resultado span#valor2').hide().html(valor_pc).fadeIn(1000)
+                    $('.resultado span#valor2').hide().html(valor_pc).fadeIn(1000) // Apresenta o valor do computador
                 }, 1000)
                 setTimeout(function () {
-                    $('.resultado span#result').hide().html(resultado).fadeIn(1000)
+                    $('.resultado span#result').hide().html(resultado).fadeIn(1000) // Apresenta a soma dos dois valores
                 }, 2000)
 
                 setTimeout(function () {
 
-                    if (resultado % 2 == 0) {
+                    if (resultado % 2 == 0) { // Verifica se o resultado deu par ou ímpar e soma os pontos do jogador que venceu
                         pontos_usuario++
-                        $('.resultado').hide().html(resultado + ' é par! Você ganhou!').fadeIn(500)
+                        $('.resultado').hide().html(resultado + ' é par! Você venceu!').fadeIn(500)
                     } else {
                         pontos_pc++
-                        $('.resultado').hide().html(resultado + ' é ímpar! O computador ganhor!').fadeIn(500)
+                        $('.resultado').hide().html(resultado + ' é ímpar! O computador venceu!').fadeIn(500)
                     }
                 }, 3000)
-                setTimeout(function () {
+
+                setTimeout(function () { // Imprime os pontos dos jogadores
                     $('#valor_ponto_user').html(pontos_usuario)
                     $('#valor_ponto_pc').html(pontos_pc)
                 }, 4000)
 
 
-
-
-
             } else {
                 alert('Digite um valor entre 0 e 10')
+                $('#valor_usuario').val('')
             }
 
         }
